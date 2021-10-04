2c60066fb3f277d57ee0b45f6b98b34130e3c208
faro <facundo.m.rodriguez1993@gmail.com>
2021-10-03 21:07:14 -0300

    cambios para que no se repita elemento

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
-function mostrarResultados(results) {
-  const contenedor = document.querySelector(".results");
-  const template = document.querySelector("#item-template");
-
-  const titulo = template.content.querySelector(".title");
-  titulo.textContent = results.title;
-
-  const fecha = template.content.querySelector(".date");
-  fecha.textContent = results.date;
-
-  const descripcion = template.content.querySelector(".description");
-  descripcion.textContent = results.explanation;
-
-  const imagen = template.content.querySelector(".img-universe");
-  imagen.src = results.url;
-
-  const clone = document.importNode(template.content, true);
-  contenedor.appendChild(clone);
-}
-
-function main() {
+function search() {
   //Guardo la key en una variable.
   const Nasa_api = "sAHEZfKbERLuQxx26A8eYFLD3FTBlADkEDeWNkJZ";
-
   const formEl = document.querySelector(".search-form");
   formEl.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -34,6 +13,28 @@ function main() {
       .then((response) => response.json())
       .then((data) => mostrarResultados(data));
   });
+}
+
+function mostrarResultados(results) {
+  const contenedor = document.querySelector(".results");
+  const template = document.querySelector("#item-template");
+  const titulo = template.content.querySelector(".title");
+  titulo.textContent = results.title;
+  const fecha = template.content.querySelector(".date");
+  fecha.textContent = results.date;
+  const descripcion = template.content.querySelector(".description");
+  descripcion.textContent = results.explanation;
+  const imagen = template.content.querySelector(".img-universe");
+  imagen.src = results.url;
+  const clone = document.importNode(template.content, true);
+  if (contenedor.firstElementChild !== null) {
+    contenedor.removeChild(contenedor.firstElementChild);
+  }
+  contenedor.appendChild(clone);
+}
+
+function main() {
+  search();
 }
 
 main();
